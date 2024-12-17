1550c1dc64e37c6f66cf75e84d9aa0530a586d7a
AdamGrochulski <adam.grochulskiii@gmail.com>
2024-12-17 21:01:59 +0100

    Wgranie plikow programu.

---
+++ src/gauss.c
@@ -0,0 +1,10 @@
+#include "gauss.h"
+
+
+/**
+ * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ */
+int eliminate(Matrix *mat, Matrix *b) {
+    int mat_r = mat->r;
+}
