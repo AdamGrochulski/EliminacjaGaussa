e5ba8a7d8522770965db06a35d23df4a8b926a0f
ChaosKoder <kzbucki7@gmail.com>
2024-12-19 21:50:57 +0100

    Student A - rozszerzenie funkcji eliminacji Gaussa
    Dodanie funkcji pivotseeker

--- src/gauss.c
+++ src/gauss.c
@@ -6,6 +6,27 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+void pivotseeker(Matrix *mat, Matrix *b, int k){
+	unsigned int m_r = mat->r;
+	unsigned int m_c = mat->c;
+	int pivotseek=0;
+	int maxpivotrow=k;
+	double maxpivot=0;
+	while (pivotseek<m_r-k){
+			if (abs(mat->data[k+pivotseek][k])>maxpivot){
+				maxpivot=abs(mat->data[k+pivotseek][k]);
+				maxpivotrow=k+pivotseek;
+			}
+		}
+	double* temporary=mat->data[k];
+	mat->data[k]=mat->data[maxpivotrow];
+	mat->data[maxpivotrow]=temporary;
+	double temporary2=b->data[k][0];
+	b->data[k][0]=b->data[maxpivotrow][0];
+	b->data[maxpivotrow][0]=temporary2;
+}
+
 int eliminate(Matrix *mat, Matrix *b){
 	unsigned int m_r = mat->r;
 	unsigned int m_c = mat->c;
@@ -16,6 +37,7 @@ int eliminate(Matrix *mat, Matrix *b){
 	double first;
 
 	while (k<m_r || k<m_c ) {
+		pivotseeker(mat,b,k);
 		pivot = mat->data[k][k];
 		i = k+1;  
 		while(i<m_c){
